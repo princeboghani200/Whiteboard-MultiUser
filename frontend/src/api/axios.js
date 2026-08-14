0a32dfb54413513d36ccba2524eb752758ff0338
princeboghani200 <k90976697@gmail.com>
2026-08-14 12:30:07 +0530

    deployment problem

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://whiteboard-backend-nfzb.onrender.com",
+  baseURL: "https://whiteboard-backend-nfzb.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
